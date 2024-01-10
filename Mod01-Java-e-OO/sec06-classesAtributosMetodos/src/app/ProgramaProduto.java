4a8d473cc8cb288ddaecd91901cfa5e4b6a29c2d
Marcia Gameleira <gameleira.mag@hotmail.com>
2024-01-09 21:32:49 -0300

    .

---
+++ Mod01-Java-e-OO/sec06-classesAtributosMetodos/src/app/ProgramaProduto.java
@@ -0,0 +1,37 @@
+package app;
+
+import java.util.Scanner;
+
+import entidades.Product;
+
+public class ProgramaProduto {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Product prodA = new Product();
+		
+		System.out.println("Informe o nome do produto: ");
+		prodA.setName(sc.nextLine());
+		System.out.println("Informe o preço do produto: ");
+		prodA.setPrice(sc.nextDouble());
+		System.out.println("Informe a quantidade do produto: ");
+		prodA.setQuantity(sc.nextInt());
+		
+		System.out.printf("R$ %.2f%n%n", prodA.totalValueInStock());
+
+		System.out.println("Adicione mais quantidade.. ");
+		prodA.addProduct(sc.nextInt());
+		System.out.printf("Estoque atualizado: %d kgs. %n", prodA.getQuantity());
+		System.out.printf("Estoque(R$) atualizado: R$ %.2f%n%n", prodA.totalValueInStock());
+		
+		System.out.println("Retire mais quantidade.. ");
+		prodA.removeProduct(sc.nextInt());
+		System.out.printf("Estoque atualizado: %d kgs. %n", prodA.getQuantity());
+		System.out.printf("Estoque(R$) atualizado: R$ %.2f%n%n", prodA.totalValueInStock());
+		
+		sc.close();		
+	}
+
+}
--- Mod01-Java-e-OO/sec06-classesAtributosMetodos/src/app/ProgramaTriangulo.java
+++ Mod01-Java-e-OO/sec06-classesAtributosMetodos/src/app/ProgramaTriangulo.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 import entidades.Triangulo;
 
-public class Programa {
+public class ProgramaTriangulo {
 	
 	public static void main(String[] args) {
 		

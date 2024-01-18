0f8e1aab0cb32ab58ed4a35491150d3d84326bfb
Marcia Gameleira <gameleira.mag@hotmail.com>
2024-01-17 23:00:50 -0300

    .

---
+++ Mod01-Java-e-OO/sec08-MemoriaVetoresListas/src/exercicios/Ex01.java
@@ -0,0 +1,32 @@
+package exercicios;
+
+import java.util.Scanner;
+
+// TRABALHANDO COM VETORES
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int qtdNumeros = sc.nextInt();
+		double[] listaDeIdades = new double[qtdNumeros];
+		double soma = 0;
+		double mediaIdades;
+		
+		for(int i = 0; i < qtdNumeros; i++) {
+			System.out.printf("Informe a %dª idade: ", i + 1);
+			listaDeIdades[i] = sc.nextDouble();
+		}
+		
+		for(int i = 0; i < qtdNumeros; i++) {
+			soma += listaDeIdades[i];
+		}
+		
+		mediaIdades = soma/qtdNumeros;
+		System.out.printf("Média: %.2f", mediaIdades);
+		sc.close();
+		
+	}
+
+}
